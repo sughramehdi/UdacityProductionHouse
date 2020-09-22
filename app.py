8e9c79b8c34b4b703fef35e201544268e31554b3
sughramehdi <sughra.mehdi@gmail.com>
2020-09-22 21:56:12 +0500

    updating app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import json
 from flask_cors import CORS
 
 from models import setup_db, Actor, Movie, CastDetails
@@ -14,21 +15,23 @@ def create_app(test_config=None):
 
     cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    # CORS Headers
-    @app.after_request
-    def after_request(response):
-        response.headers.add(
-            'Access-Control-Allow-Headers',
-            'Content-Type,Authorization,true')
-        response.headers.add(
-            'Access-Control-Allow-Methods',
-            'GET,POST,DELETE,PATCH,OPTIONS')
-        return response
-
     return app
 
 
 APP = create_app()
+
+# CORS Headers
+
+
+@APP.after_request
+def after_request(response):
+    response.headers.add(
+        'Access-Control-Allow-Headers',
+        'Content-Type,Authorization,true')
+    response.headers.add(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,PATCH,OPTIONS')
+    return response
 
 
 @APP.route('/actors')
